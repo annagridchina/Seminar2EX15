3aa6452353db2bd5d70183b8c97a44c7625f4c71
anna <annagridchina@mail.ru>
2022-12-24 15:21:14 +0700

    работает. сложнее всего правильно все заисать

--- Program.cs
+++ Program.cs
@@ -4,6 +4,25 @@
 // НО дня с индексом 0 или 12 не существует.
 // 6 -> да
 // 7 -> да
-// 1 -> нет
+// 1 -> нет// 
+
 
 Console.WriteLine("Start");
+
+Console.Write("введите число дня недели: ");
+
+int day =  int.Parse(Console.ReadLine());
+
+if (day > 0 && day < 6)
+{
+    Console.WriteLine("рабочий день");
+}
+else if (day > 5 && day < 8)
+{
+    Console.WriteLine("выходной день");
+}
+else
+{
+     Console.WriteLine("такого дня нет");
+}
+ Console.WriteLine("END");
